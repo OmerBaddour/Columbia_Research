a8d0ef126650a00174a842ba310fa4770a9cd4c9
Omer <ob2267@columbia.edu>
2020-03-04 17:32:08 -0500

    modified to now write config.txt instead of printing

--- config_creator.py
+++ config_creator.py
@@ -2,10 +2,12 @@ import math
 
 if __name__ == '__main__':
 
+    config = open("config.txt", "w")
+
     for quality in range(0, 11):
-        line = str(quality) + ": {\"codec\":\"opus\",\"packet_time\":20,\"noise\":null,\"noise_level\":0,"
-        loss_and_corrupt = 5 * math.ceil((10 - quality) / 2)
+        line = str(quality) + ": {\"codec\":\"opus\",\"packet_time\":20,\"noise\":null,\"noise_level\":0,\"mic_level\":100,\"speaker_level\":100,"
+        loss_and_corrupt = 40 - (4 * quality)
         line += "\"tx_loss\":" + str(loss_and_corrupt) + ","
         line += "\"tx_error\":" + str(loss_and_corrupt) + ","
-        line += "\"tx_delay\":" + str((10 - quality) * 30) + "}"
-        print(line)
+        line += "\"tx_delay\":" + str((10 - quality) * 30) + "}\n"
+        config.write(line)
